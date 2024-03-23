8d05a61a0f964da1c3eaf33fd00aedade17db11f
Vinit Arora <157700935+VINITARORA-21@users.noreply.github.com>
2024-03-24 02:19:53 +0530

    Update sevensegment.v

--- sevensegment/sevensegment.v
+++ sevensegment/sevensegment.v
@@ -1,3 +1,4 @@
+//common annode
 module sevensegment(
 input [1:0] status,
 output reg[6:0] seg
@@ -5,8 +6,8 @@ output reg[6:0] seg
 always @(status)
 begin
 case(status)
-2'b01:seg =7'b1000111;
-2'b10:seg =7'b1000001;
+2'b01:seg =7'b1000111; // lock
+2'b10:seg =7'b1000001; // unlock 
 endcase
 end
 
